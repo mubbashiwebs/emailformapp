d4206e4a00d3995905fedd5b2a455227c9eff28e
mubbashiwebs <mubbashirwebs@gmail.com>
2024-07-31 01:00:04 -0700

    MODIFY index js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cors());
-const upload = multer({ dest: 'uploads/' });
+// const upload = multer({ dest: 'uploads/' });
 
 
 
