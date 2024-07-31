9ae72b0fbea2e145b66df66d089a69abe01fc542
mubbashiwebs <mubbashirwebs@gmail.com>
2024-07-31 00:46:56 -0700

    add fuctions in index.js

--- index.js
+++ index.js
@@ -7,9 +7,12 @@ app.use(express.urlencoded({extended:true}));
 app.use(cors());
 
 app.get("/", (req,res)=>{
-       res.send
+       res.send('hello world')
 })
 
+app.get("/ping", (req,res)=>{
+    res.send('pong')
+})
 let port = process.env.PORT || 2000;
 app.listen(port, ()=>{
     console.log(`Server is running on port ${port}`);
